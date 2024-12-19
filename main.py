9cb4c234c1d2d176dc1584aaa92191344b8cec92
trials_id <test@trial.com>
2024-12-19 17:06:50 +0530

    allow_unsafe_werkzeug=True to allow non-prod py script as a sytem service

--- main.py
+++ main.py
@@ -316,4 +316,4 @@ if __name__ == "__main__":
     
     
     # Run the Flask app
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
